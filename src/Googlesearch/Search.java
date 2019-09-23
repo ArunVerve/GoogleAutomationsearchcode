75913108bb12a5fa54c85a08f3dbb4c7a8075c82
verve <arun.verve93@gmail.com>
2019-09-23 17:28:28 +0530

    Chrome driver latest version updated code and fixed luanched issue.

--- src/Googlesearch/Search.java
+++ src/Googlesearch/Search.java
@@ -11,16 +11,24 @@ public class Search {
 	WebDriver driver;
 	
 	@Test
-	public void OpenBrowser() throws Exception 
-	  {
-		  
-		  System.setProperty("webdriver.chrome.driver", "D:\\Arun\\su\\chromedriver.exe");
-			driver = new ChromeDriver();
+	//public void OpenBrowser() throws Exception 	
+	public void OpenBrowser() throws Exception
+	
+	{
+		  System.setProperty("webdriver.chrome.driver","D:\\Arun\\driverchrome\\chromedriver.exe");
+		  //System.setProperty("webdriver.chrome.driver", "D:\\Arun\\su\\chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", "D:\\Arun\\su\\chromedriver.exe");
+		    driver=new ChromeDriver();
+			//driver = new ChromeDriver();
+			//driver.manage().window().maximize();
 			driver.manage().window().maximize();
+			//driver.manage().deleteAllCookies();
+			driver.get("https://www.google.co.in");
+			
 			//driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 			//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			driver.manage().deleteAllCookies();
-			driver.get("https://www.google.co.in");
+			//driver.manage().deleteAllCookies();
+			//driver.get("https://www.google.co.in");
 			
 			
 		WebElement vis	=driver.findElement(By.name("q"));
